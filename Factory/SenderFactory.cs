304e8bb1e7c03175b7a565735950b604585dcdb6
UNIVERSE\eklimenko <eugene.klimenko@shift4.com>
2024-12-03 11:07:36 +0200

    Factory is added

---
+++ Factory/SenderFactory.cs
@@ -0,0 +1,28 @@
+﻿using MessageSenderQuestions.SenderAbstractions;
+using MessageSenderQuestions.Senders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageSenderQuestions.Factories
+{
+    public class SenderFactory
+    {
+        public IMessageSender CreateMessageSender(string senderType)
+        {
+            switch (senderType)
+            {
+                case "tcp":
+                    return new TcpMessageSender();
+                case "http":
+                    return new HttpMessageSender();
+                case "file":
+                    return new FileMessageSender();
+                default:
+                    throw new ArgumentException("Wrong sender type.");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,25 +1,13 @@
 ﻿using MessageSenderQuestions;
+using MessageSenderQuestions.Factories;
+using MessageSenderQuestions.SenderAbstractions;
 using MessageSenderQuestions.Senders;
 
-string senderType = args[0];
+string message = args[0];
+string senderType = args[1];
+IMessageSender sender = new SenderFactory().CreateMessageSender(senderType);
+await sender.SendAsync(message);
 
-switch (senderType)
-{
-    case "tcp":
-        var tcpMessageSender = new TcpMessageSender();
-        await tcpMessageSender.SendAsync("Something");
-        break;
-    case "http":
-        var httpMessageSender = new HttpMessageSender();
-        await httpMessageSender.SendAsync("Something");
-        break;
-    case "file":
-        var fileMessageSender = new FileMessageSender();
-        await fileMessageSender.SendAsync("Something");
-        break;
-    default:
-        break;
-}       
 
 
 
